5b8aea25ca57b98fc3311087ee66d16ad8b43260
diana.roseroba <diana.roseroba@campusucc.edu.co>
2024-04-03 14:56:34 -0500

    [update] update entities

--- src/main/java/com/eventsBooking/events/infraestructure/entities/event.java
+++ src/main/java/com/eventsBooking/events/infraestructure/entities/event.java
@@ -1,5 +1,144 @@
 package com.eventsBooking.events.infraestructure.entities;
 
-public class event {
+import java.util.Date;
 
+public class event {
+    private int id;
+    private String name;
+    private String description;
+    private String location;
+    private Date start;
+    private Date finish;
+    private Integer ageRestriction;
+    private Integer capacity;
+    private String type;
+    private String city;
+    private String address;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public event() {
+    }
+
+    public event(int id, String name, String description, String location, Date start, Date finish, Integer ageRestriction, Integer capacity, String type, String city, String address, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.start = start;
+        this.finish = finish;
+        this.ageRestriction = ageRestriction;
+        this.capacity = capacity;
+        this.type = type;
+        this.city = city;
+        this.address = address;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // Getters y setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getFinish() {
+        return finish;
+    }
+
+    public void setFinish(Date finish) {
+        this.finish = finish;
+    }
+
+    public Integer getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(Integer ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- src/main/java/com/eventsBooking/events/infraestructure/entities/ticket.java
+++ src/main/java/com/eventsBooking/events/infraestructure/entities/ticket.java
@@ -1,4 +1,114 @@
 package com.eventsBooking.events.infraestructure.entities;
 
+import java.util.Date;
+
 public class ticket {
+    private int id;
+    private Integer status;
+    private String type;
+    private Float price;
+    private Integer number;
+    private Integer presale;
+    private Date createdAt;
+    private Date updatedAt;
+    private int eventId; // Esta será la clave foránea de la tabla event
+    private int customerId; // Esta será la clave foránea de la tabla customer
+
+    public ticket() {
+    }
+
+    public ticket(int id, Integer status, String type, Float price, Integer number, Integer presale, Date createdAt, Date updatedAt, int eventId, int customerId) {
+        this.id = id;
+        this.status = status;
+        this.type = type;
+        this.price = price;
+        this.number = number;
+        this.presale = presale;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.eventId = eventId;
+        this.customerId = customerId;
+    }
+
+    // Getters y setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getPresale() {
+        return presale;
+    }
+
+    public void setPresale(Integer presale) {
+        this.presale = presale;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
 }
